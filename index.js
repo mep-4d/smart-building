60ded012b8e79a45fceb26c2de7c81aa90c10282
Attain Energy <93514502+mep-4d@users.noreply.github.com>
2024-11-13 10:22:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,22 @@ const app = Vue.createApp({
             });
         },
 
+    syntaxHighlight(json) {
+        json = JSON.stringify(json, undefined, 4);
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            let cls = 'number';
+            if (/^"/.test(match)) {
+                cls = /:$/.test(match) ? 'key' : 'string';
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    },
+
         getAssetEndpoints() {
             const url = 'https://attain.aeronlabs.com/assetEndpointsA';
             fetch(url).then(res => {
